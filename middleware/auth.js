b195214756d7b6eec2d1ceff5ffed8f20489ab85
Julianto <juliwibowo@outlook.com>
2020-04-08 23:05:15 +0700

    login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -46,3 +46,53 @@ exports.registrasi = function(req,res){
         }
     })
 }
+
+//controller untuk login
+exports.login = function(req,res){
+    var post = {
+    password: req.body.password,
+    email: req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["t_user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query,table);
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else {
+            if(rows.length == 1){
+                var token = jwt.sign({rows}, config.secret, {
+                    expiresIn: 1440
+                });
+                id_user = rows[0].id_user;
+
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["t_level"];
+
+                query = mysql.format(query, table);
+                connection.query(query, data , function(error, rows){
+                    if(error){
+                        console.log(error);
+                    }else {
+                        res.json({
+                            success: true,
+                            message: "Token JWT Tergenerate!",
+                            token:token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            }else {
+                res.json({"Error": true, "Message":"Email atau password salah!"});
+            }
+        }
+    });
+}
