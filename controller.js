d7ddafb3aa22999dcaad6a16dce23651fbe52e5a
Julianto <juliwibowo@outlook.com>
2020-04-16 14:45:15 +0700

    Berhasil menambah data montir sparepart level service user

--- controller.js
+++ controller.js
@@ -51,3 +51,127 @@ exports.tampildatamontirid = function (req,res){
         }
     });
 };
+//menambahkan data montir
+exports.tambahmontir = function (req, res) {
+    var nama_montir = req.body.nama_montir;
+    var harga_perjam = req.body.harga_perjam;
+    
+
+    connection.query('INSERT INTO t_montir (nama_montir,harga_perjam) VALUES(?,?)',
+        [nama_montir, harga_perjam],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data", res)
+            }
+        });
+};
+//menambahkan data Sparepart
+exports.tambahsparepart = function (req, res) {
+    var nama_sparepart = req.body.nama_sparepart;
+    var harga_sparepart = req.body.harga_sparepart;
+    var satuan = req.body.satuan;
+    
+
+    connection.query('INSERT INTO t_sparepart (nama_sparepart,harga_sparepart,satuan) VALUES(?,?,?)',
+        [nama_sparepart, harga_sparepart,satuan], 
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data", res)
+            }
+        });
+};
+
+//menambahkan data User
+exports.tambahuser = function (req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var role = req.body.role;
+    var tanggal_daftar = new Date();
+    
+
+    connection.query('INSERT INTO t_user (username, email, password, role, tanggal_daftar) VALUES(?,?,?,?,?)',
+        [username, email, password, role, tanggal_daftar], 
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data", res)
+            }
+        });
+};
+
+//menambahkan data User
+exports.tambahlevel = function (req, res) {
+    var nama_level = req.body.nama_level;
+    var role = req.body.role;
+    
+    
+
+    connection.query('INSERT INTO level (nama_level, role) VALUES(?,?)',
+        [nama_level, role], 
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data", res)
+            }
+        });
+};
+
+//menambahkan data service
+exports.tambahservice = function (req, res) {
+    var tgl_service = new Date();
+    var id_user = req.body.id_user;
+    var id_montir = req.body.id_montir;
+    var jumlah_sparepart = req.body.jumlah_sparepart;
+    var id_sparepart = req.body.id_sparepart;
+    var jam_service = req.body.jam_service;
+    
+
+    connection.query('INSERT INTO t_service (tgl_service, id_user, id_montir, jumlah_sparepart, id_sparepart, jam_service) VALUES(?,?,?,?,?,?)',
+        [tgl_service, id_user, id_montir, jumlah_sparepart, id_sparepart, jam_service], 
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data", res)
+            }
+        });
+};
+
+
+//Mengubah Data berdasarkan id
+exports.ubahMahasiswa = function (req, res) {
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?', [nim, nama, jurusan, id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Ubah Data", res)
+            }
+        });
+};
+
+
+//Menghapus data berdasarkan id
+exports.hapusMahasiswa = function(req, res){
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok("Berhasil Hapus Data", res)
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -18,4 +18,19 @@ module.exports = function(app){
     app.route('/tampilmontir/:id')
     .get(jsonku.tampildatamontirid);
 
+    app.route('/tambahmontir')
+    .post(jsonku.tambahmontir);
+    
+    app.route('/tambahsparepart')
+    .post(jsonku.tambahsparepart);
+
+    app.route('/tambahuser')
+    .post(jsonku.tambahuser);
+
+    app.route('/tambahlevel')
+    .post(jsonku.tambahlevel);
+
+    app.route('/tambahservice')
+    .post(jsonku.tambahservice);
+
 }
